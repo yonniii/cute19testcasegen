3bc59bb87093a8977bad0e4bd3fbd4bc42b622bd
yonnii <rkdtjdus753@gmail.com>
2021-04-02 02:10:05 +0900

    construct main controller

---
+++ src/cute19/main/main.java
@@ -0,0 +1,19 @@
+package cute19.main;
+
+import cute19.testcasegen.main.TestMain;
+import cute19.interpreter.interpreter.CuteInterpreter;
+
+import java.io.IOException;
+
+class main{
+    public static void main(String[] args){
+        String genedFilename = "out";
+        TestMain.main(new String[]{genedFilename});
+        try {
+            CuteInterpreter.main(new String[]{genedFilename});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
