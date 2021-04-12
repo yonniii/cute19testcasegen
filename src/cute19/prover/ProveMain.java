c0a4a3c1944f2510cd8632d9d7e92411fb008006
yonniii <rkdtjdus753@gmail.com>
2021-04-12 16:25:01 +0900

    write comments

--- src/cute19/prover/ProveMain.java
+++ src/cute19/prover/ProveMain.java
@@ -47,25 +47,27 @@ public class ProveMain {
         String genedCode = program.trim();
 
         for (int i = 0; i < totalInputSize; i++) {
-            programToRun = target.getPrecondition(i);
+            programToRun = target.getPrecondition(i); // get codes to set precondition
             if(programToRun == null){
                 continue;
             }
             programToRun.add(genedCode);
             List<String> results;
             try{
-                results = CuteInterpreter.callInterpreter(programToRun);
+                results = CuteInterpreter.callInterpreter(programToRun); // exec interpreter for generated program
             } catch (Exception e){
 //                e.printStackTrace();
                 continue;
             }
             String lastResult;
+            // check whether exec result is valid
             if(results.size()>0){
                 lastResult = results.get(results.size()-1).trim();
             } else {
                 continue;
             }
 
+            // If the execution result of the generated program is the same as the output specification, the generated program is returned.
             if(isReasonable(lastResult, (target.out[i][0]).toString())){
 //                System.out.println(genedCode);
                 if(i == totalInputSize-1){
@@ -83,6 +85,7 @@ public class ProveMain {
         return null;
     }
 
+//    Checks whether the result of executing the generated program is the same as the output specification
     private static boolean isReasonable(String gened, String target) {
         return gened.equals(target);
     }
@@ -96,6 +99,7 @@ class Program{
     int eachInputSize;
     int eachOutputSize;
 
+//    set input and output to field
     public boolean setInOut(Object[][] in, Object[][] out, int eachInputSize, int eachOuputSize){
         if(in.length != out.length){
             return false;
@@ -119,6 +123,7 @@ class Program{
         return true;
     }
 
+//    Before run generated program, define variants for input spec.
     public List<String> getPrecondition(int ioCount){
         String prame = "( %s %s %s )";
         List<String> codes = new ArrayList<>();
